5e6de94dbc455c3d7ec58f7bea3b494314bd70c8
kimhamney <kimhamney@gmail.com>
2023-12-17 13:56:44 +0900

    add youtube search

--- converter/views.py
+++ converter/views.py
@@ -1,42 +1,66 @@
+import os
 from django.shortcuts import render
 from django.http import JsonResponse
-from pytube import YouTube
+from django.conf import settings
 from pathlib import Path
-import os
-
-downloads_path = str(Path.home() / "Downloads")
+from pytube import YouTube
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 def main(request):
     if request.method == 'POST':
-        try:
-            url = request.POST.get('url')
-            response = "success : " + url
-            
-            url = 'https://youtu.be/RHNY1hEHdIo?si=oRgoH26NyMHqGoQN'
+        DEVELOPER_KEY= settings.YOUTUBE_API_KEY
+        YOUTUBE_API_SERVICE_NAME='youtube'
+        YOUTUBE_API_VERSION='v3'
 
-            youtube = YouTube(url)
-            
-            video = youtube.streams.filter(only_audio=True).first()
+        youtube=build(YOUTUBE_API_SERVICE_NAME,YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
 
-            fileName = getFileName(youtube.title)
-            filePath = f'{downloads_path}\{fileName}'
-            video.download(downloads_path)
-            
-            os.rename(f'{downloads_path}\{video.default_filename}', filePath)
+        search_response=youtube.search().list(
+            q="잠시라도우리",
+            order='relevance',
+            part='snippet',
+            type="video",
+            maxResults=5
+            ).execute()
+        
+        titles = []
 
-            return JsonResponse({'response': response})
-        except Exception as e:
-            return JsonResponse({'response': e})
+        for i in search_response['items']:
+            titles.append(i['snippet']['title'])
+            print(i['snippet']['title'])
+        
+        return JsonResponse({'response': search_response['items']})
 
     return render(request, 'index.html')
 
+def downloadMp3(request):
+    try:
+        url = request.POST.get('url')
+        response = "success : " + url
+            
+        url = 'https://youtu.be/RHNY1hEHdIo?si=oRgoH26NyMHqGoQN'
 
-def getFileName(_fileName):
+        youtube = YouTube(url)
+            
+        video = youtube.streams.filter(only_audio=True).first()
+
+        downloads_path = str(Path.home() / "Downloads")
+        fileName = getFileName(downloads_path, youtube.title)
+        filePath = f'{downloads_path}\{fileName}'
+        video.download(downloads_path)
+        
+        os.rename(f'{downloads_path}\{video.default_filename}', filePath)
+
+        return JsonResponse({'response': response})
+    except Exception as e:
+        return JsonResponse({'response': e})
+
+def getFileName(path, _fileName):
     fileNo = 0
     fileName = f'{_fileName}.mp3'
     while(True):
         fileNo += 1
-        filePath = f'{downloads_path}\{fileName}'
+        filePath = f'{path}\{fileName}'
         if os.path.exists(filePath):
             fileName = f'{_fileName}({fileNo}).mp3'
         else:
--- django_converter/settings.py
+++ django_converter/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+YOUTUBE_API_KEY = env('YOUTUBE_API_KEY')
